eb5a43790ad6500d51cc2783ac49d2b2cbae1bdc
Manoj <manoj09633@gmail.com>
2023-01-08 23:59:45 +0530

    Product model implemented

--- controllers/products.js
+++ controllers/products.js
@@ -1,8 +1,11 @@
+const Product = require("../models/products");
+
 exports.getAddProduct = (req, res, next) => {
   res.render("admin", { pageTitle: "Add product" });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect("/");
 };
--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const Product = require("../models/products");
 const adminRoute = require("./admin");
 
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.render("shop", { pageTitle: "Shop", prods: adminRoute.products });
+  res.render("shop", { pageTitle: "Shop", prods: Product.fetchAll() });
 });
 
 exports.routes = router;
