db62aaf5f0bd276d11e94b23f55917ffbe101076
Manoj <manoj09633@gmail.com>
2023-01-16 09:32:31 +0530

    database definition and model creation using sequelize

--- models/products.js
+++ models/products.js
@@ -1,29 +1,59 @@
-const rootDir = require("../util/path");
-const Cart = require("./cart");
-const db = require("../util/database");
+// const rootDir = require("../util/path");
+// const Cart = require("./cart");
+// const db = require("../util/database");
 
-module.exports = class Product {
-  constructor(id, title, imageURL, description, price) {
-    this.title = title;
-    this.imageURL = imageURL;
-    this.description = description;
-    this.price = price;
-  }
+// module.exports = class Product {
+//   constructor(id, title, imageURL, description, price) {
+//     this.title = title;
+//     this.imageURL = imageURL;
+//     this.description = description;
+//     this.price = price;
+//   }
 
-  save() {
-    return db.execute(
-      "INSERT INTO PRODUCTS (title,price,description,imageURL) VALUES (?,?,?,?) ",
-      [this.title, this.price, this.description, this.imageURL]
-    );
-  }
+//   save() {
+//     return db.execute(
+//       "INSERT INTO PRODUCTS (title,price,description,imageURL) VALUES (?,?,?,?) ",
+//       [this.title, this.price, this.description, this.imageURL]
+//     );
+//   }
 
-  static fetchAll() {
-    return db.execute("SELECT * FROM PRODUCTS");
-  }
+//   static fetchAll() {
+//     return db.execute("SELECT * FROM PRODUCTS");
+//   }
 
-  static fetchById(id) {
-    return db.execute("SELECT * FROM PRODUCTS WHERE PRODUCTS.id=?", [id]);
-  }
+//   static fetchById(id) {
+//     return db.execute("SELECT * FROM PRODUCTS WHERE PRODUCTS.id=?", [id]);
+//   }
 
-  static deleteById(id) {}
-};
+//   static deleteById(id) {}
+// };
+
+const Sequelize = require("sequelize");
+const sequelize = require("../util/database");
+
+const Product = sequelize.define("products", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  imageURL: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
+
+module.exports = Product;
