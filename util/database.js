826250256672ac4f3ff6b92b847c086ff23689c3
Manoj <manoj09633@gmail.com>
2023-01-18 22:31:18 +0530

    Cart model created

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("node_complete", "root", "", {
+const sequelize = new Sequelize("node_complete", "root", "xy753@bhK", {
   dialect: "mysql",
   host: "localhost",
 });
