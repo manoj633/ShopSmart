a48a9b68e6315ba80b7572df1de3b1281c815b98
Manoj <manoj09633@gmail.com>
2023-01-18 23:36:25 +0530

    Associations defined between user, product, and order

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const Product = require("./models/products");
 const User = require("./models/user");
 const Cart = require("./models/cart");
 const CartItem = require("./models/cart-item");
+const Order = require("./models/order");
+const OrderItem = require("./models/order-item");
 
 const app = new express();
 
@@ -38,6 +40,11 @@ Cart.belongsTo(User);
 
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+
+Order.belongsTo(User);
+User.hasMany(Order);
+
+Order.belongsToMany(Product, { through: OrderItem });
 
 app.use("/", (req, res, next) => {
   res.redirect("/shop/");
